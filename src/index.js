bdccb38f5a0d69444122a8b9ad7a4cf53765df2d
Rick Maya <rickmaya@gmail.com>
2021-01-20 12:53:28 -0600

    import STORE component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import STORE from "./STORE";
 
 ReactDOM.render(
-    <App />,
+    <App store={STORE} />,
   document.getElementById('root')
 );
 
